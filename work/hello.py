b0a5bb1908e54af803cea1a007c83d018157428e
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2019-01-06 10:41:37 +0100

    Add GitIgnore | Add Hello World example

---
+++ work/hello.py
@@ -0,0 +1,48 @@
+print("Hello World!")
+print('Hello World!')
+print('''I am mad in love
+            do you think
+                I am doing
+                    the right thing''')
+print("""I am mad in love
+            do you think
+                I am doing
+                    
+                        the right thing""")
+
+# Single line comment
+
+"""
+Multi line comment
+"""
+
+# Escaping characters
+print("Hey there it's a cow")
+print("Hello World 'Mr' Bond")
+print('old world "but" still good')
+
+# Escape seuquences start with \ in P3, not in P2!
+print('\a')
+print('\tHermit')
+print("i know, they are 'great'")
+
+# String Concatenation
+print("Only way " + "to join two strings")
+
+# Formatted output
+# %d = decimal, %s = string, %f = float
+# %5d.2 = 5 spaces, 2 precision
+print("Name \t\t\tMarks \t\t\tAge")
+print("%s %14.2f %11d" % ("John Doe", 80.67, 27))
+print("%s %12.2f %11d" % ("Jane Doe", 79.901, 27))
+print("%s %3.2f %11d" % ("Jack Doe", 56.98, 25))
+
+
+# Indentation
+
+def fun():
+    pass
+
+
+for each in "Australia":
+    pass
